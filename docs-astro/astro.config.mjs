b54932d21002d97fa64c72f304c68f2c28382e56
Aidan Hembree <8049680+ahembree@users.noreply.github.com>
2026-02-04 23:23:07 -0500

    Update astro.config.mjs

--- docs-astro/astro.config.mjs
+++ docs-astro/astro.config.mjs
@@ -35,7 +35,7 @@ export default defineConfig({
 					tag: 'meta',
 					attrs: {
 						property: 'og:title',
-						content: 'Home Media Server deployment tool - HMS-Docker'
+						content: 'HMS-Docker - A Home Media Server deployment orchestrator'
 					}
 				},
 				{
